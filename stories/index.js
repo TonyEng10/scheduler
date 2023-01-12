2a64616b3f02c761e5d8b5a12a2517c27b974276
Tony Eng <tony.eng@telus.com>
2023-01-11 19:27:58 -0800

    add status component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -165,5 +166,10 @@ storiesOf("Button", module)
               message="Delete the apopintment?"
               onConfirm={action("onConfirm")}
               onCancel={action("onCancel")}
+            />
+          ))
+          .add("Status", () => (
+            <Status 
+              message="Deleting"
             />
           ))
